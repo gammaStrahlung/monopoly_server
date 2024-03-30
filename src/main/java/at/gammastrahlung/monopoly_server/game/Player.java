26b6c86c676f43d51653746ba8e1bddb6aa624ea
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-03-30 01:09:12 +0100

    Add Player name field

--- src/main/java/at/gammastrahlung/monopoly_server/game/Player.java
+++ src/main/java/at/gammastrahlung/monopoly_server/game/Player.java
@@ -3,9 +3,22 @@ package at.gammastrahlung.monopoly_server.game;
 import java.util.UUID;
 
 public abstract class Player {
+    /**
+     * The unique ID of the player
+     */
     private UUID ID;
 
+    /**
+     * The name of the player (this is shown to other players)
+     */
+    private String name;
+
+    // Getters and Setters below:
     public UUID getID() {
         return ID;
+    }
+
+    public String getName() {
+        return name;
     }
 }
