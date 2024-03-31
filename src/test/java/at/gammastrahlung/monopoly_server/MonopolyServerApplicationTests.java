79dc5752cfd832deb58a67b2e665f9ef316d2a6e
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-04-01 00:55:57 +0200

    Test Game and check for MonopolyController

--- src/test/java/at/gammastrahlung/monopoly_server/MonopolyServerApplicationTests.java
+++ src/test/java/at/gammastrahlung/monopoly_server/MonopolyServerApplicationTests.java
@@ -1,13 +1,21 @@
 package at.gammastrahlung.monopoly_server;
 
+import at.gammastrahlung.monopoly_server.network.websocket.MonopolyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class MonopolyServerApplicationTests {
 
+	@Autowired
+	private MonopolyController monopolyController;
+
 	@Test
 	void contextLoads() {
+        assertNotNull(monopolyController);
 	}
 
 }
