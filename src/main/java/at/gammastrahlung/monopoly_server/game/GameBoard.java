d26d749868640c1019d606213da5f7ea58c9b258
erlmatt <merlacher2001@gmail.com>
2024-04-19 12:11:33 +0200

    added constructor to Property class

--- src/main/java/at/gammastrahlung/monopoly_server/game/GameBoard.java
+++ src/main/java/at/gammastrahlung/monopoly_server/game/GameBoard.java
@@ -1,12 +1,14 @@
 package at.gammastrahlung.monopoly_server.game;
 
 public class GameBoard {
+
+    private Player Bank = new Player();
     private Field[] gameBoard;
 
 
     void inializeGameBoard(){
         gameBoard[0] = new Field(0, "LOS!");
-        gameBoard[1] = Field.builder().build();
+        gameBoard[1] = new Property(1, "Minimundus", Bank, 100, 5, 10, 15, 20, 30);
 
     }
 
--- src/main/java/at/gammastrahlung/monopoly_server/game/Property.java
+++ src/main/java/at/gammastrahlung/monopoly_server/game/Property.java
@@ -2,6 +2,7 @@ package at.gammastrahlung.monopoly_server.game;
 
 import java.awt.event.FocusEvent;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,6 +23,25 @@ public class Property extends Field{
     private int twoHousesRent;
     private int threeHousesRent;
     private int hotelRent;
+
+    public Property(int fieldID, String name, Player owner, int price, int baseRent, int oneHouseRent, int twoHousesRent, int threeHousesRent, int hotelRent) {
+        super(fieldID, name);
+        this.owner = owner;
+        this.price = price;
+        this.baseRent = baseRent;
+        this.oneHouseRent = oneHouseRent;
+        this.twoHousesRent = twoHousesRent;
+        this.threeHousesRent = threeHousesRent;
+        this.hotelRent = hotelRent;
+    }
+
+    private void buyAndSellProperty(Player buyer){
+        owner.addBalance(price);
+        buyer.subtractBalance(price);
+        this.owner = buyer;
+    }
+
+
 
 
 }
