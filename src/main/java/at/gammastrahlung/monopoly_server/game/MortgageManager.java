9892a0d7bd7b80a1a0fb15673864d80bcea16c99
MauMauChat <lucaan@edu.aau.at>
2024-04-28 20:39:32 +0200

    Add MortgageManager class for managing mortgage operations
    This commit adds the MortgageManager class to the game package. The MortgageManager is responsible for handling mortgage operations within the Monopoly game.
    
    The class includes:
    - A private field `gameBoard` of type `GameBoard`, allowing access to properties on the game board.
    - A constructor that initializes the MortgageManager with a provided game board.
    
    This class lays the foundation for implementing mortgage functionality in the game.

---
+++ src/main/java/at/gammastrahlung/monopoly_server/game/MortgageManager.java
@@ -0,0 +1,35 @@
+package at.gammastrahlung.monopoly_server.game;
+
+import at.gammastrahlung.monopoly_server.game.gameboard.Property;
+import at.gammastrahlung.monopoly_server.game.gameboard.GameBoard;
+
+/**
+ * Manages mortgage operations within the game.
+ */
+public class MortgageManager {
+
+    private GameBoard gameBoard;
+
+    /**
+     * Constructor that initializes the MortgageManager with a game board.
+     * @param gameBoard The game board used to access properties.
+     */
+    public MortgageManager(GameBoard gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
