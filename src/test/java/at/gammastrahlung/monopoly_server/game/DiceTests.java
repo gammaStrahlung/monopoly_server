14a01fbd33e3018a3f88e7ece8f2cdc7311b334b
marrabl <162188192+marrabl@users.noreply.github.com>
2024-05-02 13:19:09 +0200

    Add tests for Dice class

---
+++ src/test/java/at/gammastrahlung/monopoly_server/game/DiceTests.java
@@ -0,0 +1,31 @@
+package at.gammastrahlung.monopoly_server.game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class DiceTests {
+    private Dice dice;
+
+    @BeforeEach
+    public void setUp() {
+        dice = new Dice();
+    }
+
+    @Test
+    public void constructorTest(){
+        assertEquals(1, dice.getValue1());
+        assertEquals(5, dice.getValue2());
+    }
+
+    @Test
+    public void initializeDiceTest(){
+        dice.initializeDice();
+
+        assertTrue(dice.getValue1() >= 1 && dice.getValue1() <= 6);
+        assertTrue(dice.getValue2() >= 1 && dice.getValue2() <= 6);
+    }
+}
