778bff2a8074f8ef68378892f7fed7b3d322af39
majasmolej <smolej.9@gmail.com>
2024-05-08 22:00:11 +0200

    fix method no of args

--- src/test/java/at/gammastrahlung/monopoly_server/game/FieldActionHandlerTests.java
+++ src/test/java/at/gammastrahlung/monopoly_server/game/FieldActionHandlerTests.java
@@ -1,25 +1,28 @@
 package at.gammastrahlung.monopoly_server.game;
 
 import at.gammastrahlung.monopoly_server.game.gameboard.FieldType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.UUID;
-
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class FieldActionHandlerTests {
 
-    @Test
-    void goToJail(){
-        Player mockPlayer = mock(Player.class);
+    Player mockPlayer;
+
+    @BeforeEach
+    void initialize(){
+        mockPlayer = mock(Player.class);
         when(mockPlayer.getId()).thenReturn(UUID.randomUUID());
         when(mockPlayer.getName()).thenReturn("Player");
+    }
 
-        Game game = mock(Game.class);
-
-        FieldActionHandler.handleFieldAction(FieldType.GO_TO_JAIL, mockPlayer, game);
+    @Test
+    void goToJail(){
+        FieldActionHandler.handleFieldAction(FieldType.GO_TO_JAIL, mockPlayer);
 
         verify(mockPlayer).goToJail();
     }
