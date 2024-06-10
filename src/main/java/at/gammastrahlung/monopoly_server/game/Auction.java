98afcdee48901eec6b4b0181b2f041bcef80fc49
MauMauChat <lucaan@edu.aau.at>
2024-06-10 23:32:32 +0200

    Add evaluateBids method to Auction class
    - Added a method to evaluate the bids for the auction
    - The method adds a new bid, determines the highest bid, and assigns the property to the winning player

--- src/main/java/at/gammastrahlung/monopoly_server/game/Auction.java
+++ src/main/java/at/gammastrahlung/monopoly_server/game/Auction.java
@@ -6,18 +6,39 @@ import at.gammastrahlung.monopoly_server.game.Player;
 import at.gammastrahlung.monopoly_server.game.gameboard.Field;
 import at.gammastrahlung.monopoly_server.game.gameboard.GameBoard;
 import at.gammastrahlung.monopoly_server.game.gameboard.Property;
+import lombok.Data;
 
 import java.util.List;
 import java.util.UUID;
-
+ @Data
 public class Auction {
+
     private List<Bid> bids;
     private Game game;
     private GameBoard gameBoard;
 
+    public void evaluateBids(Bid newBid) {
+        bids.add(newBid); // Add the new bid to the list of bids
 
+        Bid highestBid = null;
+        Player winningPlayer = null;
 
+        for (Bid bid : bids) {
+            if (highestBid == null || bid.getAmount() > highestBid.getAmount()) {
+                highestBid = bid;
+                winningPlayer = game.getPlayerById(bid.getPlayerId());
+            }
+        }
 
+        if (highestBid != null) {
+            Field field = game.getGameBoard().getFieldByIndex(highestBid.getFieldIndex());
+            if (field instanceof Property) {
+                Property property = (Property) field;
+                property.setOwner(winningPlayer);
+            }
+        }
+    }
+}
 
 
 
