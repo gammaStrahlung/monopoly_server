8fd708b7822f710ed29e0893764800465b0524ff
MauMauChat <lucaan@edu.aau.at>
2024-06-10 23:39:18 +0200

    Create Bid class and add necessary imports
    - Created Bid class under the game package
    - Imported necessary classes from lombok and java.util
    Add class variables to Bid class
    
    - Added a UUID to hold the player's ID
    - Added an integer to hold the bid amount
    - Added an integer to hold the field index

---
+++ src/main/java/at/gammastrahlung/monopoly_server/game/Bid.java
@@ -0,0 +1,16 @@
+package
+at.gammastrahlung.monopoly_server.game;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Data
+public class Bid {
+    private UUID playerId; // The ID of the player who made the bid
+    private int amount; // The amount of the bid
+    private int fieldIndex; // The field the bid is made on
+
+
+}
