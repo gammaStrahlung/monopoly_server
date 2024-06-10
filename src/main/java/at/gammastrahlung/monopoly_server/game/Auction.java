119acf7afc7d567a1a1b50ffc3e77da1c4b8fdab
MauMauChat <lucaan@edu.aau.at>
2024-06-10 23:30:57 +0200

    Create Auction class and add necessary imports
    - Created Auction class under the game package
    - Imported necessary classes from the game and gameboard packages
    - Imported List and UUID from java.util

---
+++ src/main/java/at/gammastrahlung/monopoly_server/game/Auction.java
@@ -0,0 +1,34 @@
+ package at.gammastrahlung.monopoly_server.game;
+
+import at.gammastrahlung.monopoly_server.game.Bid;
+import at.gammastrahlung.monopoly_server.game.Game;
+import at.gammastrahlung.monopoly_server.game.Player;
+import at.gammastrahlung.monopoly_server.game.gameboard.Field;
+import at.gammastrahlung.monopoly_server.game.gameboard.GameBoard;
+import at.gammastrahlung.monopoly_server.game.gameboard.Property;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Auction {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
